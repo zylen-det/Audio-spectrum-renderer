33e9111975d33085e15ef28e1f1f29dc5971bdc8
zhongdet <thomas980930@gmail.com>
2026-05-24 12:28:10 +0800

    vite --host for dev server

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
+import basicSsl from "@vitejs/plugin-basic-ssl"
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), basicSsl()],
   server: {
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
